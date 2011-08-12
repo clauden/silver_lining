162e7e3262aaee434d6665d71b2ed48523073c1d
Claude Noshpitz <claude.noshpitz@att.com>
2011-08-11 19:18:39 -0700

    Actually do the scan.

--- portscan/run.bash
+++ portscan/run.bash
@@ -4,5 +4,5 @@
 range=$1
 range=${range:-"10.4.1.0/24"}   # this is palo alto vm network (?)
 
-echo sudo nmap "$range" -v -oX /tmp/nmap-$$.xml
+sudo nmap "$range" -oX /tmp/nmap-$$.xml
 ruby parse.rb /tmp/nmap-$$.xml 
