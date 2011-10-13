69ec10bcc351b0296a1dcd25009ec016e2056216
Claude Noshpitz <claude.noshpitz@att.com>
2011-10-13 13:44:56 -0700

    wrapper

--- portscan/run.bash
+++ portscan/run.bash
@@ -4,5 +4,7 @@
 range=$1
 range=${range:-"10.4.1.0/24"}   # this is palo alto vm network (?)
 
-sudo nmap "$range" -oX /tmp/nmap-$$.xml
-ruby parse.rb /tmp/nmap-$$.xml 
+outfile="/tmp/nmap-$$.xml"
+sudo nmap -v -oX $outfile "$range" 
+ruby parse.rb $outfile
+sudo rm $outfile
